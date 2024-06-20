24b648efd3dd401361e615350d049b839be556b0
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-20 13:13:30 +0530

    Solved issues in two sums and submitted the code

--- twoSum.c
+++ twoSum.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* twoSum(int* nums, int target, int numsSize);
+int* twoSumsAdjacent(int* nums, int target, int numsSize, int* returnSize);
+int* twoSums(int* nums, int numsSize, int target, int* returnSize);
 
 int main(void) {
-    int nums[] = {2,7,11,5};
-    int target = 9;
+    int nums[] = {3, 2, 4};
+    int target = 6;
     int numSize = 4;
-    int *result = twoSum(nums, target, numSize);
+    int returnSze = 2;
+    int *returnSize = &returnSze;
+
+    int *result = twoSums(nums, numSize, target, returnSize);
     for (int i = 0; i < 2; i ++) {
-        printf("%d\n", result[i]);
+        printf("number %d is %d\n", i, result[i]);
     }
 
     free(result);
 }
 
-int* twoSum(int* nums, int target, int numsSize) {
+int* twoSumsAdjacent(int* nums, int target, int numsSize, int* returnSize) {
     int *result = (int*) malloc(2 * sizeof(int));
 
     for (int i = 0; i < numsSize; i++ ) {
@@ -25,5 +29,31 @@ int* twoSum(int* nums, int target, int numsSize) {
             break;
         }
     }
+
+    *returnSize = 2;
+
+    return result;
+}
+
+int* twoSums(int* nums, int numsSize, int target, int* returnSize) {
+    int *result = (int*) malloc(2 * sizeof(int));
+    int resulter = 0;
+    for (int i = 0; i < numsSize; i++ ) {
+        if (resulter == 1) {
+            break;
+        }
+        for (int j = 0; j < numsSize; j++) {
+            if (i == j) {
+                continue;;
+            }
+            if (nums[i] + nums[j] == target) {
+                *result = i;
+                *(result + 1) = j;
+                resulter = 1;
+                break;
+            }
+        }
+    }
+    *returnSize = 2;
     return result;
 }
